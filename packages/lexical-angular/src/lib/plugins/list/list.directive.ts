6837d937192208ecada8eaf32e6508782d95b568
riccardoperra <riccardo.perra@icloud.com>
2022-04-17 23:01:13 +0200

    feat: add lexical list plugin

---
+++ packages/lexical-angular/src/lib/plugins/list/list.directive.ts
@@ -0,0 +1,78 @@
+import {Directive, OnDestroy, OnInit} from '@angular/core';
+import {LexicalController} from '../../lexical.controller';
+import {Unsubscribable} from 'rxjs';
+import {mergeRegister} from '@lexical/utils';
+import {
+  INDENT_CONTENT_COMMAND,
+  INSERT_PARAGRAPH_COMMAND,
+  OUTDENT_CONTENT_COMMAND,
+} from 'lexical';
+import {
+  $handleListInsertParagraph,
+  indentList,
+  INSERT_ORDERED_LIST_COMMAND,
+  INSERT_UNORDERED_LIST_COMMAND,
+  insertList,
+  outdentList,
+  REMOVE_LIST_COMMAND,
+  removeList,
+} from '@lexical/list';
+import {COMMAND_PRIORITY_LOW} from '../../constants/priorities';
+
+@Directive({selector: '[lexicalComposer][lexicalList]'})
+export class LexicalListDirective implements OnInit, OnDestroy {
+  listener: Unsubscribable | null = null;
+
+  constructor(private readonly controller: LexicalController) {}
+
+  ngOnInit(): void {
+    const {editor} = this.controller;
+    this.listener = {
+      unsubscribe: mergeRegister(
+        editor.registerCommand(
+          INDENT_CONTENT_COMMAND,
+          () => indentList(),
+          COMMAND_PRIORITY_LOW
+        ),
+        editor.registerCommand(
+          OUTDENT_CONTENT_COMMAND,
+          () => outdentList(),
+          COMMAND_PRIORITY_LOW
+        ),
+        editor.registerCommand(
+          INSERT_ORDERED_LIST_COMMAND,
+          () => {
+            insertList(editor, 'ol');
+            return true;
+          },
+          COMMAND_PRIORITY_LOW
+        ),
+        editor.registerCommand(
+          INSERT_UNORDERED_LIST_COMMAND,
+          () => {
+            insertList(editor, 'ul');
+            return true;
+          },
+          COMMAND_PRIORITY_LOW
+        ),
+        editor.registerCommand(
+          REMOVE_LIST_COMMAND,
+          () => {
+            removeList(editor);
+            return true;
+          },
+          COMMAND_PRIORITY_LOW
+        ),
+        editor.registerCommand(
+          INSERT_PARAGRAPH_COMMAND,
+          () => $handleListInsertParagraph(),
+          COMMAND_PRIORITY_LOW
+        )
+      ),
+    };
+  }
+
+  ngOnDestroy(): void {
+    this.listener?.unsubscribe();
+  }
+}
---
+++ packages/lexical-angular/src/lib/plugins/list/list.module.ts
@@ -0,0 +1,8 @@
+import {LexicalListDirective} from './list.directive';
+import {NgModule} from '@angular/core';
+
+@NgModule({
+  exports: [LexicalListDirective],
+  declarations: [LexicalListDirective],
+})
+export class LexicalListPluginModule {}
