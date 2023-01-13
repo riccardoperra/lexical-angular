27eac30d6b48194396a7d192c79d339f6445e50c
Giorgio Azzinnaro <giorgio@azzinna.ro>
2023-01-13 14:58:43 +0100

    fix(playground): tests

--- packages/playground/src/app/app.component.spec.ts
+++ packages/playground/src/app/app.component.spec.ts
@@ -22,13 +22,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('playground');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome playground'
-    );
-  });
 });
